0360947348068d31ef7e04cb75d1b6ba8deeb01a
Martin Cervantes <cervantes.martine@gmail.com>
2020-06-17 09:05:30 -0600

    Added new email regex validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ class User < ApplicationRecord
   has_many :comments, dependent: :destroy
 
   validates :username, length: { in: 4..12 }, presence: true, uniqueness: true
-  validates :email, format: { with: /\A(\S+)@(.+)\.(\S+)\z/ }, presence: true, uniqueness: true
+  validates :email, format: { with: /\A[\w+\-.]+@[\w+\-.]*\.[a-z]+\z/i }, presence: true, uniqueness: true
   validates :password, length: { in: 6..16 }, presence: true
 end
